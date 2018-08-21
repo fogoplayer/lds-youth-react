aacf67c51f2f5af1601455d3bdb7cb17ca9e3873
fogoplayer <fogoplayer@gmail.com>
2018-08-21 14:52:44 -0600

    Add notes

---
+++ src/components/pages/duty-to-god/appendix/notes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import TextArea from "../../../TextArea";
+
+const Page = props => (
+  <div>
+    <TextArea id="notes" />
+  </div>
+);
+
+export default Page;
