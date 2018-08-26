fb1caad29944bcecddb297396a900340d3653cf6
fogoplayer <fogoplayer@gmail.com>
2018-08-26 09:20:09 -0600

    Add materialize as local file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, BrowserHistory } from "react-router-dom";
 import App from "./components/App";
+import "./materialize.min.css";
+import "./materialize.min.js";
 
 ReactDOM.render(
   <BrowserRouter history={BrowserHistory}>
