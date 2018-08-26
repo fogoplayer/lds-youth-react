f9eaa3440f1ecf9abcd62f6f9d71f19b41bc507c
Zarin Loosli <fogoplayer@gmail.com>
2018-08-26 22:00:35 +0000

    Fix issues with compiler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, BrowserHistory } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-  <BrowserRouter history={BrowserHistory}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
