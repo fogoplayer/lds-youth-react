be8247ab820f1edba8c328f6e005f000b538628c
fogoplayer <fogoplayer@gmail.com>
2018-08-26 09:11:39 -0600

    Add scripture index

---
+++ src/components/pages/personal-progress/scripture-index.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Page = props => (
+  <div>
+    <h2>Scripture Index</h2>
+    <p>
+      You will read many of these scriptures as you complete your value
+      experiences and value projects. We encourage you to mark them all and note
+      the associated Young Women value in your scriptures. You may wish to also
+      note those that are part of seminary scripture mastery verses.
+    </p>
+    <h4>Faith</h4>
+    Isaiah 53:3-12 Malachi 3:8-12 Matthew 26:26-28 Mark 14:22-24 Luke 22:17-20
+    John 3:16-17 Romans 5 1 Corinthians 15:22 Hebrews 11 Revelation 12:7-9 2
+    Nephi 9:1-28 2 Nephi 11:4-7 Alma 7:11-13 Alma 32:17-43 Alma 34:8-17 Alma
+    56:45-48 Alma 57:21 Ether 12:6-22 D&C 19:15-20 D&C 76:50-113 D&C 93:33-34
+    D&C 119 Moses 4:1-4 Abraham 3:24-27 Joseph Smith-History 1:11-20
+    <h4>Divine Nature</h4>
+    Proverbs 31:10-31 Matthew 5:9 Luke 2:40-51 John 6:38 John 15:12 Galatians
+    5:22-23 Colossians 3:12-17 2 Peter 1 1 John 4:21 Alma 7:23-24 Moroni 7:44-48
+    D&C 20:77, 79 D&C 38:27 D&C 121:45
+    <h4>Individual Worth</h4>
+    Psalm 8:4-6 Jeremiah 1:5 John 13:34 1 Corinthians 12:4-12 1 Corinthians 13
+    Moroni 7:12-13 Moroni 10:8-18 D&C 18:10 D&C 46:11-26 D&C 88:119 D&C 121:45
+    Abraham 3:22-23 Joseph Smith- History 1:1-20 Knowledge Proverbs 1:5 Proverbs
+    4:7 Matthew 25:14-30 2 Nephi 28:30 D&C 88:78-80 D&C 88:118 D&C 90:15 D&C
+    130:18–19 D&C 131:6 Articles of Faith 1:13
+    <h4>Choice and Accountability</h4>
+    Joshua 24:15 Isaiah 1:18 Ezekiel 36:26–27 John 14:26 John 16:13 Galatians
+    5:22–25 1 Nephi 15:8 2 Nephi 2 2 Nephi 9:51 2 Nephi 32:3 2 Nephi 32:5 Alma
+    26:22 Alma 34:19–27 Alma 34:30–35 Ether 2–3 Moroni 8:25–26 Moroni 10:4–5 D&C
+    9:7–9 D&C 11:12–14 D&C 19:15–20 D&C 58:42–43 D&C 82:2–10 Moses 4:1–4 Moses
+    7:32
+    <h4>Good Works</h4>
+    Matthew 5:13–16 Matthew 24:14 Matthew 25:34–40 Matthew 28:19 Galatians
+    6:9–10 James 1:22–27 Mosiah 2:17 Mosiah 4:26 Mosiah 18:7–10 3 Nephi 12:16 3
+    Nephi 13:1–4 D&C 58:26–28 D&C 88:81
+    <h4>Integrity</h4>
+    Genesis 39 Esther Job 2:3 Job 27:3–6 Daniel 3 Daniel 6 Acts 26 Mosiah 18:9 3
+    Nephi 11:10–11 Moroni 10:30–33 D&C 124:15 Joseph Smith—History 1:21–25
+    <h2>Virtue</h2>
+    Proverbs 31:10–31 John 14:26–27 John 15:26 2 Nephi 32:1–5 Jacob 2:28 Enos
+    Alma 5 Moroni 10:32–33 D&C 20:77, 79 D&C 45:57–59 D&C 88:3–4 D&C 121:45–46
+    Articles of Faith 1:13
+  </div>
+);
+
+export default Page;
