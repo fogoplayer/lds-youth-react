f22063f967d950c639e5da448ae6a264d139bfcf
fogoplayer <fogoplayer@gmail.com>
2018-08-26 15:44:50 -0600

    Update packages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, BrowserHistory } from "react-router-dom";
 import App from "./components/App";
-import "./materialize.min.css";
-import "./materialize.min.js";
 
 ReactDOM.render(
   <BrowserRouter history={BrowserHistory}>
