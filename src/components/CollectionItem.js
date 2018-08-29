7bc7b16c09c2f1073e7a04160f71a27f237c106b
Zarin Loosli <fogoplayer@gmail.com>
2018-08-29 16:42:35 +0000

    Links fill collection items

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const CollectionItem = props => (
   <li className="collection-item">
     <Link to={props.url}>
+    <div>
       <h5 style={{ color: "black" }}>{props.text}</h5>
+      </div>
     </Link>
   </li>
 );
