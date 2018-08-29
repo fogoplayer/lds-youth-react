537b5294418e407e132f222d21fbcefb168ee24a
Zarin Loosli <fogoplayer@gmail.com>
2018-08-29 14:55:53 +0000

    Continued progress with scripture-index

--- src/components/pages/personal-progress/scripture-index.js
+++ src/components/pages/personal-progress/scripture-index.js
@@ -8,9 +8,9 @@ const Page = props => (
             experiences and value projects. We encourage you to mark them all and note
             the associated Young Women value in your scriptures. You may wish to also
             note those that are part of seminary scripture mastery verses.
-        </p>
+</p>
         <div className="row">
-            {/* <div className="col s12 m6 l3">
+            <div className="col s12 m6 l3">
                 <h4>Faith</h4>
                 <p><a href="https://www.lds.org/scriptures/ot/isa/53.3-12">Isaiah 53:3-12 </a></p>
                 <p><a href="https://www.lds.org/scriptures/ot/mal/3.8-12">Malachi 3:8-12</a></p>
@@ -81,7 +81,7 @@ const Page = props => (
                 <p><a href="https://lds.org/scriptures/dc-testament/dc/88.78-80">D&C 88:78-80</a></p>
                 <p><a href="https://lds.org/scriptures/dc-testament/dc/88.118">D&C 88:118</a></p>
                 <p><a href="https://lds.org/scriptures/dc-testament/dc/90.15">D&C 90:15</a></p>
-                <p><a href="https://lds.org/scriptures/dc-testament/dc/130.18-19">D&C 130:18–19</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/130.18-19">D&C 130:18-19</a></p>
                 <p><a href="https://lds.org/scriptures/dc-testament/dc/131.6">D&C 131:6</a></p>
                 <p><a href="https://lds.org/scriptures/pgp/a-of-f/1.13">Articles of Faith 1:13</a></p>
             </div>
@@ -89,78 +89,88 @@ const Page = props => (
                 <h4>Choice and Accountability</h4>
                 <p><a href="https://lds.org/scriptures/ot/josh/24.15">Joshua 24:15</a></p>
                 <p><a href="https://lds.org/scriptures/ot/isa/1.18">Isaiah 1:18</a></p>
-                <p><a href="https://lds.org/scriptures/ot/ezek/36.26-27">Ezekiel 36:26–27</a></p>
+                <p><a href="https://lds.org/scriptures/ot/ezek/36.26-27">Ezekiel 36:26-27</a></p>
                 <p><a href="https://lds.org/scriptures/nt/john/14.26">John 14:26</a></p>
                 <p><a href="https://lds.org/scriptures/nt/john/16.13">John 16:13</a></p>
-                <p><a href="https://lds.org/scriptures/nt/gal/5.22-25">Galatians 5:22–25</a></p>
+                <p><a href="https://lds.org/scriptures/nt/gal/5.22-25">Galatians 5:22-25</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/1-ne/15.8">1 Nephi 15:8</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/2-ne/2">2 Nephi 2</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/2-ne/9.51">2 Nephi 9:51</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/2-ne/32.3">2 Nephi 32:3</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/2-ne/32.5">2 Nephi 32:5</a></p>
                 <p><a href="https://lds.org/scriptures/bofm/alma/26.22">Alma 26:22</a></p>
-                <p><a href="https://lds.org/scriptures/bofm/alma/34.19-27">Alma 34:19–27</a></p>
-                <p><a href="https://lds.org/scriptures/bofm/alma/34.30-35">Alma 34:30–35</a></p>
-                <p><a href="https://lds.org/scriptures/bofm/ether/2">Ether 2–3</a></p>
-                <p><a href="https://lds.org/scriptures/bofm/moro/8.25-26">Moroni 8 :25–26</a></p    >
-            <p><a href="https://lds.org/scriptures/bofm/moro/10.4-5">Moroni 10:4–5</a><    /p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/9.7-9">D&C 9:7–9</a    ></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/11.121-14">D&C 11:12–14<    /a></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/19.15-20">D&C 19:15–2    0</a></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/58.42-43">D&C 58:42�    �43</a></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/82.2-10">D&C 82:2    –10</a></p>
-            <p><a href="https://lds.org/scriptures/pgp/moses/4.1-4">Moses     4:1–4</a></p>
-            <p><a href="https://lds.org/scriptures/pgp/moses/7.32"    >Moses 7:32</a>    </p>
-        </div>
-        <div className="    col s12 m6 l3">
-            <h4>    Good Works</h4>
-            <p><a href="https://lds.org/scriptures/nt/matt/5.13-16">Matthew     5:13–16</a></p>
-            <p><a href="https://lds.org/scriptures/nt/matt/24.14">Matt    hew 24:14</a></p>
-            <p><a href="https://lds.org/scriptures/nt/matt/25.34-40">Matthew     25:34–40</a></p>
-            <p><a href="https://lds.org/scriptures/nt/matt/28.19">Ma    tthew 28:19</a></p>
-            <p><a href="https://lds.org/scriptures/nt/gal/6.9-10">Galat    ians 6:9–10</a></p>
-            <p><a href="https://lds.org/scriptures/nt/james/1.22-27">    James 1:22–27</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/mosiah/2.    17">Mosiah 2:17</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/mosiah/4.    26">Mosiah 4:26</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/mosiah/18.7-10"    >Mosiah 18:7–10</a></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/88.    81">3 Nephi 12:16</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/3-ne/13.1-    4">3 Nephi 13:1–4</a></p>
-            <p><a href="https://lds.org/scriptures/dc-testament/dc/58.    26-28">D&C 58:26–28</a></p>
-            <p><a href="https://lds.org/scriptures/dc-test    ament/dc/88.81"    >D&C 88:81</a></p>
-        </div>
-        <d    iv className="col s12 m6 l3">
+                <p><a href="https://lds.org/scriptures/bofm/alma/34.19-27">Alma 34:19-27</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/alma/34.30-35">Alma 34:30-35</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/ether/2">Ether 2-3</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/moro/8.25-26">Moroni 8 :25-26</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/moro/10.4-5">Moroni 10:4-5</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/9.7-9">D&C 9:7-9</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/11.121-14">D&C 11:12-14</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/19.15-20">D&C 19:15-20</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/58.42-43">D&C 58:42-43</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/82.2-10">D&C 82:2-10</a></p>
+                <p><a href="https://lds.org/scriptures/pgp/moses/4.1-4">Moses 4:1-4</a></p>
+                <p><a href="https://lds.org/scriptures/pgp/moses/7.32">Moses 7:32</a></p>
+            </div>
+            <div className="col s12 m6 l3">
+                <h4>Good Works</h4>
+                <p><a href="https://lds.org/scriptures/nt/matt/5.13-16">Matthew 5:13-16</a></p>
+                <p><a href="https://lds.org/scriptures/nt/matt/24.14">Matthew 24:14</a></p>
+                <p><a href="https://lds.org/scriptures/nt/matt/25.34-40">Matthew 25:34-0</a></p>
+                <p><a href="https://lds.org/scriptures/nt/matt/28.19">Matthew 28:19</a></p>
+                <p><a href="https://lds.org/scriptures/nt/gal/6.9-10">Galatians 6:9-10</a></p>
+                <p><a href="https://lds.org/scriptures/nt/james/1.22-27">James 1:22-27</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/mosiah/2.17">Mosiah 2:17</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/mosiah/4.26">Mosiah 4:26</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/mosiah/18.7-10">Mosiah 18:7-10</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/88.81">3 Nephi 12:16</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/3-ne/13.1-4">3 Nephi 13:1-</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/58.26-28">D&C 58:26-28</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/88.81">D&C 88:81</a></p>
+            </div>
+            <div className="col s12 m6 l3">
                 <h4>Integrity</h4>
-            <p><a href="https://lds.org/scriptures/    ot/gen/39">Genesis 39</a></p>
-            <p><a href="https://lds.org/scriptu    res/ot/esth/1">Esther</a></p>
-            <p><a href="https://lds.org/scripture    s/ot/job/2.3">Job 2:3</a></p>
-            <p><a href="https://lds.org/scriptures/ot/j    ob/27.3-6">Job 27:3–6</a></p>
-            <p><a href="https://lds.org/script    ures/ot/dan/3">Daniel 3</a></p>
-            <p><a href="https://lds.org/script    ures/ot/dan/6">Daniel 6</a></p>
-            <p><a href="https://lds.org/scriptu    res/nt/acts/26">Acts 26</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/m    osiah/18.9">Mosiah 18:9</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/3-ne/11.    10-11">3 Nephi 11:10–11</a></p>
-            <p><a href="https://lds.org/scriptures/bofm/moro/    10.30-33">Moroni 10:30–33</a></p>
-            <p><a href="https://lds.org/scriptures/dc-test    ament/dc/124.15">D&C 124:15</a></p>
-            <p><a href="https://lds.org/scriptures/pgp/js-h/1.21-2    5">Joseph Smith    —History 1:21–25</a></p>
+                <p><a href="https://lds.org/scriptures/ot/gen/39">Genesis 39</a></p>
+                <p><a href="https://lds.org/scriptures/ot/esth/1">Esther</a></p>
+                <p><a href="https://lds.org/scriptures/ot/job/2.3">Job 2:3</a></p>
+                <p><a href="https://lds.org/scriptures/ot/job/27.3-6">Job 27:3-6</a></p>
+                <p><a href="https://lds.org/scriptures/ot/dan/3">Daniel 3</a></p>
+                <p><a href="https://lds.org/scriptures/ot/dan/6">Daniel 6</a></p>
+                <p><a href="https://lds.org/scriptures/nt/acts/26">Acts 26</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/mosiah/18.9">Mosiah 18:9</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/3-ne/11.10-11">3 Nephi 11:10-11</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/moro/10.30-33">Moroni 10:30-33</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/124.15">D&C 124:15</a></p>
+                <p><a href="https://lds.org/scriptures/pgp/js-h/1.21-25">Joseph Smith-History 1:21-25</a></p>
+            </div>
+            <div className="col s12 m6 l3">
+                <h4>Virtue</h4>
+                <p><a href="https://www.lds.org/scriptures/ot/prov/31.10-31">Proverbs 31:10-31</a></p>
+                <p><a href="https://lds.org/scriptures/nt/john/14.26-27">John 14:26-27</a></p>
+                <p><a href="https://www.lds.org/scriptures/nt/john/15.26">John 15:26</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/2-ne/32.1-5">2 Nephi 32:1-5</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/jacob/2.28">Jacob 2:28</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/enos">Enos</a></p>
+                <p><a href="https://lds.org/scriptures/bofm/alma/5">Alma 5</a></p>
+                <p><a href="https://www.lds.org/scriptures/bofm/moro/10.32-33">Moroni 10:32-33</a></p>
+                <p><a href="https://www.lds.org/scriptures/dc-testament/dc/20.77">D&C 20:77</a>, <a href="https://www.lds.org/scriptures/dc-testament/dc/20.79">79</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/45.57-59">D&C 45:57-59</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/88.3-4">D&C 88:3-</a></p>
+                <p><a href="https://lds.org/scriptures/dc-testament/dc/121.45-46">D&C 121:45- 6</a></p>
+                <p><a href="https://lds.org/scriptures/pgp/a-of-f/1.13">Articles of Faith 1:13</a></p>
+            </div>
         </div>
-            <div className="col     s12 m6 l3">
-            <h4>Virtue</h4>
-            <p><a href="https://www.lds.org/scriptures/ot/p    rov/31.10-31">Proverbs 31:10-31</a></p>
-            <p><a href="https://lds.org/scriptures/    nt/john/14.26-27">John 14:26-27</a></p>
-            <p><a href="https://www.lds.org/scrip    tures/nt/john/15.26">John 15:26</a></p>
-            <p><a href="https://lds.org/scriptures/b    ofm/2-ne/32.1-5">2 Nephi 32:1–5</a></p>
-            <p><a href="https://lds.org/scrip    tures/bofm/jacob/2.28">Jacob 2:28</a></p>
-            <p><a href="https://l    ds.org/scriptures/bofm/enos">Enos</a></p>
-            <p><a href="https://lds.o    rg/scriptures/bofm/alma/5">Alma 5</a></p>
-            <p><a href="https://www.lds.org/scriptures/bo    fm/moro/10.32-33">Moroni 10:32–33</a></p>
-            <p><a href="https://www.lds.org/scriptures/dc-testament/dc/20.77">D&C 20:77</a>, <a href="https://www.lds.org/s    criptures/dc-testament/dc/20.79">79</a></p>
-            <p><a href="https://lds.org/scriptures/dc-    testament/dc/45.57-59">D&C 45:57–59</a></p>
-            <p><a href="https://lds.org/scriptur    es/dc-testament/dc/88.3-4">D&C 88:3–4</a></p>
-            <p><a href="https://lds.org/scriptures/d    c-testament/dc/121.45-46">D&C 121:45–46</a></p>
-            <p><a href="https://lds.org/scrip    tures/        */}
+    </div>
+);
+export default Page;             <p><a href="  </div>
+);
+export default Page;h 1:13</a></p>
+        </div>
+  </div>
+);
 
-        </div>
-gp  /a-of-f/1.13">Articles of Faith 1:13</a></p>
+export default Page;
+3</a></p>
         </div>
   </div>
 );
