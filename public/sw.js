9ba093228b993c118f929c1a02808d5810f4b3be
Zarin Loosli <fogoplayer@gmail.com>
2018-09-02 16:59:17 +0000

    Update offline capabilities

--- public/sw.js
+++ public/sw.js
@@ -4,16 +4,38 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
-  // workbox.routing.registerRoute(
-  //   /(.*)materialize\.min\.(?:js|css)/,
-  //   workbox.strategies.cacheFirst({
-  //     cacheName: 'materialize-cache'
-  //   })
-  // );
+  // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    }),
+  );
+
+  // Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'google-fonts-webfonts',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+
+  //Cache materialize
   workbox.precaching.precacheAndRoute([
     "/materialize.min.js",
     "/materialize.min.css"
   ]);
+
+  //Inject manifest
   workbox.precaching.precacheAndRoute([]);
 }
 else {
