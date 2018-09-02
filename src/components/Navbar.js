a0f124440b49c151012485d53ccf5de915f60a80
Zarin Loosli <fogoplayer@gmail.com>
2018-09-02 18:22:36 +0000

    Add sign in dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,41 @@
+/* global M */
 import React from "react";
 import { Link } from "react-router-dom";
+class Navbar extends React.Component { // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
 
-const Navbar = props => (
-  <div className="navbar-fixed">
-    <nav>
-      <div className="nav-wrapper black row">
-        <div className="col s12">
-          <a data-target="sidenav" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </a>
-          <Link to="/" className="brand-logo">
-            {props.brand}
-          </Link>
+    //Initialize materialize components
+    let elems = document.querySelectorAll(".dropdown-trigger");
+    M.Dropdown.init(elems, {
+      alignment: "right",
+      constrainWidth: false,
+      coverTrigger: false,
+    });
+  }
+  render() {
+    return (<div className="navbar-fixed">
+      <ul id="signinDropdown" className="dropdown-content">
+        <li><h1>Coming soon</h1></li>
+      </ul>
+      <nav>
+        <div className="nav-wrapper black row">
+          <div className="col s12">
+            <a data-target="sidenav" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <Link to="/" className="brand-logo">
+              {this.props.brand}
+            </Link>
+            <ul className="right">
+              <li><a className="dropdown-trigger" data-target="signinDropdown"><i className="material-icons left">account_circle</i>Sign In</a></li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
-);
-
+      </nav>
+    </div>);
+  }
+}
 export default Navbar;
