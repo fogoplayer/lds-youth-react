04358932d056d1e611732b1ba65cdb1db1226478
Zarin Loosli <fogoplayer@gmail.com>
2018-09-06 00:15:34 +0000

    Allow all letters in signin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,12 +5,18 @@ import React from "react";
 class Signin extends React.Component { // eslint-disable-next-line
     constructor(props) {
         super(props);
-        this.state = { currentUserBool: !!firebase.auth().currentUser};
+        this.state = { currentUserBool: !!firebase.auth().currentUser };
     }
     componentDidMount() {
         //Change state 
         firebase.auth().onAuthStateChanged(user => {
             this.setState({ currentUserBool: !!firebase.auth().currentUser });
+        });
+
+        document.querySelectorAll("input").forEach(node => {
+            node.addEventListener('keypress', event => { event.stopPropagation(); })
+            node.addEventListener('keydown', event => { event.stopPropagation(); })
+            node.addEventListener('keyup', event => { event.stopPropagation(); })
         });
     }
     render() {
@@ -20,19 +26,19 @@ class Signin extends React.Component { // eslint-disable-next-line
                     <a className="black-text" style={{width:"300px"}}>
                         <p >Sign Up with Email</p>
                         <div className="input-field">
-                            <input id="name" type="text" tabIndex="1"  onKeyDown={function(event){console.error(event);event.stopPropagation()}}/>
+                            <input id="name" type="text" tabIndex="1"/>
                             <label htmlFor="name">Name</label>
                         </div>
                         <div className="input-field">
-                            <input id="email" type="text" tabIndex="2"  onKeyDown={function(event){event.stopPropagation()}}/>
+                            <input id="email" type="text" tabIndex="2"/>
                             <label htmlFor="email">Email</label>
                         </div>
                         <div className="input-field">
-                            <input id="password1" type="password" tabIndex="3"  onKeyDown={function(event){event.stopPropagation()}}/>
+                            <input id="password1" type="password" tabIndex="3"/>
                             <label htmlFor="password1">Password</label>
                         </div>
                         <div className="input-field">
-                            <input id="password2" type="password" data-error="Passwords must match" tabIndex="4"  onKeyDown={function(event){event.stopPropagation()}}/>
+                            <input id="password2" type="password" data-error="Passwords must match" tabIndex="4"/>
                             <label htmlFor="password2">Re-enter Password</label>
                         </div>
                         <br/>
@@ -46,9 +52,10 @@ class Signin extends React.Component { // eslint-disable-next-line
                 <li className="divider"></li>
                 <li><a className="black-text" onClick={this.signInWithGoogle}>Sign up with Google</a></li>
             </ul>);
-            
-        }else if (this.state.currentUserBool === "signIn"){
-            
+
+        }
+        else if (this.state.currentUserBool === "signIn") {
+
             return (<ul id="signInDropdown" className="dropdown-content">
                 <li>
                     <a className="black-text" style={{width:"300px"}}>
@@ -72,10 +79,10 @@ class Signin extends React.Component { // eslint-disable-next-line
                 <li className="divider"></li>
                 <li><a className="black-text" onClick={this.signInWithGoogle}>Sign in with Google</a></li>
             </ul>);
-            
+
         }
         else {
-            
+
             return (<ul id="signInDropdown" className="dropdown-content">
                 <li className = "black-text"
                 onClick = {function() {
@@ -85,44 +92,45 @@ class Signin extends React.Component { // eslint-disable-next-line
                 
                 </li>
             </ul>);
-            
+
         }
     }
-    
+
     //Sign in functions
-    signUpWithEmail(){
-        console.dir("this",this);
+    signUpWithEmail() {
+        console.dir("this", this);
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password1").value;
         const password2 = document.getElementById("password2").value;
-        if(password === password2) {
+        if (password === password2) {
             firebase.auth().createUserWithEmailAndPassword(email, password).then(response => {
-                firebase.auth().currentUser.updateProfile({displayName: name});
+                firebase.auth().currentUser.updateProfile({ displayName: name });
                 window.location.reload();
             }).catch(function(error) {
-                M.toast({html: `Error: ${error.code}<br/>${error.message}`});
+                M.toast({ html: `Error: ${error.code}<br/>${error.message}` });
             });
-            
+
             M.Dropdown.getInstance(document.getElementById("signInDropdownTrigger")).close();
-        }else{
-            M.toast({html: 'Passwords must match'});
+        }
+        else {
+            M.toast({ html: 'Passwords must match' });
         }
     }
-    
-    signInWithEmail(){
+
+    signInWithEmail() {
         const email = document.getElementById("email").value;
         const password = document.getElementById("password1").value;
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            M.toast({html: `Error: ${error.code}<br/>${error.message}`});
+            M.toast({ html: `Error: ${error.code}<br/>${error.message}` });
         });
         M.Dropdown.getInstance(document.getElementById("signInDropdownTrigger")).close();
     }
-    
-    signInWithGoogle(){
+
+    signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).catch(function(error) {
-            let toast = M.toast({html:`Error: ${error.code}<br/>${error.message}`});
+            let toast = M.toast({ html: `Error: ${error.code}<br/>${error.message}` });
             toast.displayLength = 1000000;
         });
         M.Dropdown.getInstance(document.getElementById("signInDropdownTrigger")).close();
